0e150baf05d0d6175092af9ddd04b6412a9fe9dd
Egor <info@xcono.com>
2017-07-06 19:33:07 +0700

    Add django admin specific js.

---
+++ rubricate/static/rubricate/rubricate.django.js
@@ -0,0 +1,37 @@
+function DjangoRubricate(appElement, input)
+{
+    this.el = appElement;
+    this.input = input;
+
+    this.options = {
+        defaultData: input.value.length > 2 ? JSON.parse(JSON.parse(input.value)) : {}, // escaped json parsed twice
+        uploadUrl: function () {return input.getAttribute('data-upload-url')},
+        csrfToken: function () {return ''},
+    };
+
+    this.app = Rubricate(this.el, this.options);
+
+    this.getModelFormElement().onsubmit = function(){
+        this.input.value = JSON.stringify(this.app.getData());
+    }.bind(this);
+}
+
+DjangoRubricate.prototype.getModelFormElement = function()
+{
+    // get closest parent form element to stream app element
+    var el = this.el;
+    while ((el = el.parentElement) && el.tagName.toLocaleLowerCase() !== 'form') {}
+    return el;
+};
+
+window.addEventListener('load', function () {
+    [].forEach.call(document.getElementsByClassName('rubricate-input'), function (input) {
+
+        var app = document.createElement('div');
+        var form_row = input.parentNode.parentNode;
+        form_row.classList.remove('hidden');
+        form_row.appendChild(app);
+
+        new DjangoRubricate(app, input);
+    });
+});
