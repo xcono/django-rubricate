3f1a816351050fcd8e5ff1f1296777ef8ab10835
Egor <info@xcono.com>
2017-07-23 23:54:48 +0700

    Update tests settings.

--- tests/settings.py
+++ tests/settings.py
@@ -21,6 +21,7 @@ ROOT_URLCONF = 'tests.urls'
 INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.sites',
     'rubricate',
 ]
